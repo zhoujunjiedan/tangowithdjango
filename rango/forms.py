20d01c43cf72400e40df7201307b6c98e1cc898d
Firstname Surname <2099999z@student.gla.ac.uk>
2019-02-14 19:41:33 +0000

    test1

--- rango/forms.py
+++ rango/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from rango.models import Page, Category, UserProfile
 from django.contrib.auth.models import User
+
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=128, help_text="Please enter the category name.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
